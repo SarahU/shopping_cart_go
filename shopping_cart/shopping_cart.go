9d6fd05777f8abb268ca539210ba7f47b73f9185
Juan Amari <juan@snyk.io>
2021-10-14 11:46:09 +0300

    refactor: shopping_cart to ShoppingCart, formatting

--- shopping_cart/shopping_cart.go
+++ shopping_cart/shopping_cart.go
@@ -1,17 +1,17 @@
 package shopping_cart
 
-type shoppingcart struct {
-    total int
+type ShoppingCart struct {
+	total int
 }
 
-func (shopping_cart *shoppingcart) getTotal() int{
-    return shopping_cart.total
+func (shopping_cart *ShoppingCart) getTotal() int {
+	return shopping_cart.total
 }
 
-func (shopping_cart *shoppingcart) addItem(
-    itemName string,
-    quantity int,
-    cost int){
+func (shopping_cart *ShoppingCart) addItem(
+	itemName string,
+	quantity int,
+	cost int) {
 
-    shopping_cart.total = cost
+	shopping_cart.total = cost
 }
--- shopping_cart/shopping_cart_test.go
+++ shopping_cart/shopping_cart_test.go
@@ -1,27 +1,29 @@
 package shopping_cart
 
-import ( "testing" )
+import (
+	"testing"
+)
 
 func TestShoppingCart(t *testing.T) {
 
-        t.Run("shopping cart total = 0", func(t *testing.T) {
-            cart := shoppingcart{}
-            total := cart.getTotal()
+	t.Run("shopping cart total = 0", func(t *testing.T) {
+		cart := ShoppingCart{}
+		total := cart.getTotal()
 
-            if(total != 0){
-                t.Errorf("expected %q received %q", 0, total)
-            }
-        })
+		if total != 0 {
+			t.Errorf("expected %q received %q", 0, total)
+		}
+	})
 
-        t.Run("shopping cart total = 0", func(t *testing.T) {
-             cart := shoppingcart{0}
+	t.Run("shopping cart total = 0", func(t *testing.T) {
+		cart := ShoppingCart{0}
 
-             cart.addItem("Apple", 1, 20)
+		cart.addItem("Apple", 1, 20)
 
-             total := cart.getTotal()
+		total := cart.getTotal()
 
-             if(total != 20){
-                t.Errorf("expected %q received %q", 0, total)
-             }
-        })
+		if total != 20 {
+			t.Errorf("expected %q received %q", 0, total)
+		}
+	})
 }
