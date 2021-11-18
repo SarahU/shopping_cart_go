9175cfd4c74841607502b2f912a8fcd3b9fafd09
Juan Amari <juan@snyk.io>
2021-11-18 11:34:26 +0200

    6 bananas special - green

--- shopping_cart/shopping_cart.go
+++ shopping_cart/shopping_cart.go
@@ -13,20 +13,26 @@ func (shopping_cart *ShoppingCart) addItem(
 	quantity int,
 	cost int) {
 
-	if quantity == 3 && itemName == "Apple" {
-		shopping_cart.total += 130
+	if quantity >= 3 && itemName == "Apple" {
+		specials := quantity / 3
+		regular := quantity % 3
+
+		specials_price := 130 * specials
+		regular_price := cost * regular
+
+		shopping_cart.total = specials_price + regular_price
 		return
 	}
 
 	if itemName == "Banana" && quantity >= 2 {
-	    specials := quantity / 2
-	    regular := quantity % 2
+		specials := quantity / 2
+		regular := quantity % 2
 
-	    specials_price := 45 * specials
-	    regular_price := cost * regular
+		specials_price := 45 * specials
+		regular_price := cost * regular
 
-	    shopping_cart.total = specials_price+regular_price
-	    return
+		shopping_cart.total = specials_price + regular_price
+		return
 	}
 
 	shopping_cart.total += cost * quantity
